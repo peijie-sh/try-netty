20bda471b012e4d3c49bdd222292bec2c27a8dd6
Joepis <peijie-sh@outlook.com>
2018-07-20 11:34:01 +0800

    path

--- src/main/java/client/EchoClient.java
+++ src/main/java/client/EchoClient.java
@@ -1,3 +1,5 @@
+package client;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
--- src/main/java/client/EchoClientHandler.java
+++ src/main/java/client/EchoClientHandler.java
@@ -1,3 +1,5 @@
+package client;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler;
